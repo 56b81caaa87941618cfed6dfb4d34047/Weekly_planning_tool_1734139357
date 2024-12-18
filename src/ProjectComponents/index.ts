a709f8139e657be73d9098c2d2cea30fbac95935
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-18 21:51:25 +0000

    Add component: Header1734558685

--- src/ProjectComponents/index.ts
+++ src/ProjectComponents/index.ts
@@ -1,3 +1,4 @@
+export * as Header1734558685 from './Header1734558685'
 export * as Hero1734139359 from './Hero1734139359'
 export * as Header1734139359 from './Header1734139359'
 export * as Footer1734139359 from './Footer1734139359'
